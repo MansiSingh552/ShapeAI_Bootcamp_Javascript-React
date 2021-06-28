e8447a79ba430f2b0122ded98dc3e727d15bff45
MansiSingh552 <86358721+MansiSingh552@users.noreply.github.com>
2021-06-28 10:17:52 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>React</h1>
+      <p>Lets learn basic web development with Javascript and React </p>
+    </div>
+  );
+}
+
+export default Info;
